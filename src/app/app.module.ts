0de9798a667e7f1ec28f485ab910b91466630954
Oleh Tymoshenko <olegtymoshenko2000@gmail.com>
2020-11-17 19:18:45 +0200

    Add worked normal, but after adding FCM insted of firebase-messeging package in android studio start appear error with building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AlertController, IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FCM } from '@ionic-native/fcm/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,9 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AlertController,
+    FCM
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../shared/services/photo.service';
- 
+import { Plugins,
+         PushNotification,
+         PushNotificationToken,
+         PushNotificationActionPerformed,
+         Capacitor
+        } from '@capacitor/core'; 
+import { AlertController } from '@ionic/angular';
+import { FCM } from '@ionic-native/fcm/ngx';
+
+const { PushNotifications } = Plugins;
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -9,14 +19,21 @@ import { PhotoService } from '../shared/services/photo.service';
 export class Tab2Page {
 
   constructor(
-    public photoService : PhotoService
+    public photoService : PhotoService,
+    private fcm : FCM
   ) {}
 
   async ngOnInit() {
     await this.photoService.loadSaved();
+    await this.setUpPushNotifications();
   }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+  
+  private async setUpPushNotifications() {
+    this.fcm.subscribeToTopic("all");
+    alert(await this.fcm.getToken());
   }
 }
